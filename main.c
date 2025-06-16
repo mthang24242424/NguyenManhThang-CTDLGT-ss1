82918f6a96891297245abf13dd9ec6d8babdf3c1
mthang24242424 <wunkevan@gmail.com>
2025-06-16 13:45:22 +0700

    bài 1

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < n; i ++) {
+        printf("hello \n");
+    }
+    return 0;
+}
+// đoạn mã sử dụng 1 vòng lặp for in ra từ hello n lần, độ phức tạp về thời gian là O(n)
